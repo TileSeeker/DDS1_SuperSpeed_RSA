7f6c2fe5b1d2852d627e893a7234b8e6727dc6e3
Peter Knutsen <peterk98@gmail.com>
2023-11-24 11:16:16 +0100

    Multi Core Start

--- tfe4141_rsa_integration_kit_2023/Exponentiation/source/exponentiation.vhd
+++ tfe4141_rsa_integration_kit_2023/Exponentiation/source/exponentiation.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity exponentiation is
 	generic (
 		C_block_size : integer := 256;
-		cores        : integer := 2
+		core_count        : integer range 15 downto 1 := 2
 	);
 	port (
 		--input controll
@@ -60,3 +60,55 @@ begin
     e  => key,
     C  => result);
 end expBehave;
+
+architecture core_gen of exponentiation is
+signal rst: std_logic;
+signal count_in     : integer range core_count-1 downto 0 := 0;
+signal count_out    : integer range core_count-1 downto 0 := 0;
+
+signal valid_in_vector      : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+signal ready_in_vector      : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+signal valid_out_vector     : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+signal ready_out_vector     : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+signal msgin_last_vector    : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+signal msgout_last_vector   : std_logic_vector (core_count-1 downto 0) := (others=>'0');
+
+begin
+    rst <= not(reset_n);
+    
+    core_generate: for i in 0 to core_count-1 generate
+    begin
+    
+    Binary: entity work.binary(rtl)
+	generic map(
+	block_size => C_block_size)
+	port map(
+    rst         => rst,
+    clk         => clk,
+    en          => valid_in_vector(i),
+    rdy         => ready_in_vector(i),
+    valid_out   => valid_out_vector(i),
+    ready_out   => ready_out_vector(i),
+    msgin_last  => msgin_last_vector(i),
+    msgout_last => msgout_last_vector(i),
+    
+    M  => Message,
+    N  => Modulus,
+    e  => key,
+    C  => result);
+    end generate;
+    
+   mux_in_generate: for i in 0 to core_count-1 generate
+   begin
+    if (count_in = i) then
+        valid_in_vector(i)      <= valid_in;
+        ready_in_vector(i)      <= ready_in;
+        msgin_last_vector(i)    <= msgin_last;
+    end if;
+   end generate;
+   
+    
+
+
+
+end architecture;
--- tfe4141_rsa_integration_kit_2023/Exponentiation/testbench/new_binary_tb.vhd
+++ tfe4141_rsa_integration_kit_2023/Exponentiation/testbench/new_binary_tb.vhd
@@ -15,8 +15,10 @@ architecture tb of binary_tb is
     signal clk          :   std_logic := '0';
     signal en           :   std_logic := '0';
     signal rdy          :   std_logic := '0';
-    signal valid_out    :   std_logic;
+    signal valid_out    :   std_logic := '0';
     signal ready_out    :   std_logic := '0';
+    signal msgin_last   :   std_logic := '0';
+    signal msgout_last  :   std_logic := '0';
     
     signal M    :   std_logic_vector(block_size-1 downto 0) := (others=>'0');
     signal N    :   std_logic_vector(block_size-1 downto 0) := (others=>'0');
@@ -25,7 +27,9 @@ architecture tb of binary_tb is
     
     constant T: time := 1ns;
 begin
-
+---------------------------------------------------------
+    --Component
+---------------------------------------------------------
 DUT: entity work.binary(rtl)
     generic map (
     block_size => block_size)
@@ -34,74 +38,70 @@ DUT: entity work.binary(rtl)
     clk =>clk,
     en => en,
     rdy => rdy,
-    ready_out=> ready_out,
-    valid_out=> valid_out,
+    ready_out => ready_out,
+    valid_out => valid_out,
+    msgin_last => msgin_last,
+    msgout_last => msgout_last,
     M => M,
     N => N,
     e => e,
     C => C);
-    
+  
+---------------------------------------------------------
+    --CLK
+---------------------------------------------------------  
    clk <= not(clk) after T/2;
    
+   
+---------------------------------------------------------
+    --Test
+---------------------------------------------------------
    test: process is
    begin
-   rst <= '0';
-   wait for 5 ns;
-   rst <= '1';
-   en <= '0';
-   wait for 5 ns;
-   rst <= '0';
-   wait for 5 ns;
-   
+---------------------------------------------------------
+    --Reset
+---------------------------------------------------------
+   rst <= '1'; wait for T;
+   rst <= '0'; wait for T;
+
+---------------------------------------------------------
+    --Encrypt Test 1
+---------------------------------------------------------
    M <= std_logic_vector(to_unsigned(50, block_size));
    e <= std_logic_vector(to_unsigned(17, block_size));
    n <= std_logic_vector(to_unsigned(143, block_size));
-   wait for 5ns;
-   en <= '1';
-    wait for 5ns;
-
-   wait until valid_out='1';
+   
+   en <= '1'; wait for T;
+   en <= '0'; wait for T;
+   wait until valid_out;
+   ready_out <= '1'; wait for T;
+   ready_out <= '0'; wait for T;
    assert (C = std_logic_vector(to_unsigned(85, block_size))) report "Test: Modulo Operation Result Error" severity failure;
-   ready_out <= '1';    wait for T;
-   ready_out <= '0';    wait for T;
    
-   
-   rst <= '0';
-   wait for 5 ns;
-   rst <= '1';
-   en <= '0';
-   wait for 5 ns;
-   rst <= '0';
-   wait for 5 ns;
-   
-   m <= x"b64ce14712586ff4e5aa50459bc31d1c3cf7e94727067505189bc67be52baad9";
+---------------------------------------------------------
+    --Encrypt Test 2
+---------------------------------------------------------
+    --Encrypt
+   m <= x"0000000011111111222222223333333344444444555555556666666677777777";
    e <= x"0000000000000000000000000000000000000000000000000000000000010001";
-   n <= x"d7cff677f3d26cfa6d5ca63cf2ddb7d120ae8abaf11e7b833a2338ca07471bd7";
-   wait for T;
-   en <= '1';    wait for T;
+   n <= x"99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d";
    
-   wait until valid_out='1';
-   assert (C = x"d69a72752977ffdd38e5fdb4524183c3aa8c1fbb3791fcd14dd5c8551d6afcd8") report "Test: Modulo Operation Result Error" severity failure;
+   en <= '1'; wait for T;
+   en <= '0'; wait for T;
+   wait until valid_out;
+   ready_out <= '1'; wait for T;
+   ready_out <= '0'; wait for T;   
+   --Decrypt
    M <= C;
-   e <= x"005f1e74ae149e7fbf361f1fd0bd3aa69e8b66745f2d50a0b1d82caf648d05c9";
-   ready_out <= '1';    wait for T;
-   ready_out <= '0';    wait for T;
-   
-   
-   rst <= '0';
-   wait for 5 ns;
-   rst <= '1';
-   en <= '0';
-   wait for 5 ns;
-   rst <= '0';
-   wait for 5 ns;
-   en <= '1';
-   wait for 5ns;
-       
+   e <= x"0cea1651ef44be1f1f1476b7539bed10d73e3aac782bd9999a1e5a790932bfe9";
 
-   wait until valid_out='1';
-   --assert (C = std_logic_vector(to_unsigned(50, block_size))) report "Test: Modulo Operation Result Error" severity failure;
-   assert (C = x"b64ce14712586ff4e5aa50459bc31d1c3cf7e94727067505189bc67be52baad9") report "Test: Modulo Operation Result Error" severity failure;
+   en <= '1'; wait for T;
+   en <= '0'; wait for T;
+   wait until valid_out;
+   ready_out <= '1'; wait for T;
+   ready_out <= '0'; wait for T; 
+   assert (C = x"0000000011111111222222223333333344444444555555556666666677777777") report "Test: Modulo Operation Result Error" severity failure;
+   
    assert false report "Test: OK" severity failure;
    end process test;
   
